46af699d4b809d16e974950ce7dd3578a9c79e4a
Harshu <harshadaawhale1122@gmail.com>
2022-06-09 14:32:14 +0530

    third commit

--- index.php
+++ index.php
@@ -89,14 +89,14 @@ $con->close();
 
               
 
-              <li><div class="bottom-area">
+              <!-- <li><div class="bottom-area">
                 <div class="container">
                   <button class="toggle-btn">
                     <i class="far fa-moon"></i>
                     <i class="far fa-sun"></i>
                   </button>
                 </div>
-              </div></li>
+              </div></li> -->
             </ul>
           </div>
 
